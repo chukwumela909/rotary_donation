f265f4ab2a6804fa8a00a38f5a1ae0539958771f
chukwumela909 <amirizew@gmail.com>
2024-10-04 11:52:02 +0100

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready( function(){
+    $("#switch").click(function(){
+        $(".hero_text").fadeToggle()
+    })
+})
+
+
+
+
+
+function add_two_number(num1, num2){
+    var result = num1 + num2
+    return result
+}
