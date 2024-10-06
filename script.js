af415591cd11bf746a3073522fc1002da94a458d
chukwumela909 <amirizew@gmail.com>
2024-10-06 06:40:33 +0100

    jkj

--- script.js
+++ script.js
@@ -17,7 +17,19 @@ function register() {
     const b_pic = baby_picture.files[0];
     const b_card = baby_card.files[0];
 
-    if (!b_pic || b_card) {
+
+        // Validate input (add your validation logic here)
+        if (baby_name == "" || baby_age == "" || baby_gender == "" || baby_picture == "" || baby_card == "") {
+            hideLoader()
+            return Swal.fire({
+                title: "Missing fields",
+                text: "Input fields cannot be left blank",
+                icon: "question",
+    
+            });
+        }
+
+    if (!b_pic || !b_card) {
         hideLoader()
         return Swal.fire({
              title: "Upload Image",
@@ -28,98 +40,112 @@ function register() {
     } 
 
     showLoader()
-    // Validate input (add your validation logic here)
-    if (baby_name == "" || baby_age == "" || baby_gender == "" || baby_picture == "" || baby_card == "") {
-        hideLoader()
-        return Swal.fire({
-            title: "Missing fields",
-            text: "Input fields cannot be left blank",
-            icon: "question",
 
-        });
-    }
 
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append('b_pic', b_pic);
+    formData.append('b_card', b_card);
     formData.append('upload_preset', 'khday3qd');
-
-    axios.post('https://api.cloudinary.com/v1_1/daf6mdwkh/image/upload', formData)
-        .then(response => {
-
-
-
-
-            // console.log(response.data.url)
-            // Send registration request to the server using Axios
-            axios.post('https://shalomserver.onrender.com/admin/create-plan', {
-                name: name,
-                targetinvestor: targetInvestor,
-                targetcash: targetCash,
-                targetmultiple: targetMultiple,
-                photo: `${response.data.url}`
-            })
-                .then(response => {
-                    console.log(response)
-
-                    // Handle successful registration response
-
-                    if (response.data['error'] == true) {
-                        hideLoader()
-                        return Swal.fire({
-                            title: "Error",
-                            text: response.data['message'],
-
-
-                            icon: "error"
-                        });
-                    }
-
-                    hideLoader()
-
-                    return Swal.fire({
-                        title: "Success",
-                        text: 'Successful',
-
-
-                        icon: "success"
-                    });
-
-                })
-                .catch(error => {
-                    hideLoader()
-                    return Swal.fire({
-                        title: "Error",
-                        text: error,
-
-
-                        icon: "error"
-                    });
+    window.location.pathname = "enroll_success.html";
+    // axios.post('https://api.cloudinary.com/v1_1/daf6mdwkh/image/upload', formData)
+    //     .then(response => {
 
 
 
-                })
-                .finally(() => {
-                    // Hide the loader when the request is complete (success or failure)
-                    hideLoader()
-                });
-        })
-        .catch(error => {
-            hideLoader()
-            return Swal.fire({
-                title: "Error",
-                text: error,
+
+    //         // console.log(response.data.url)
+    //         // Send registration request to the server using Axios
+    //         axios.post('https://shalomserver.onrender.com/admin/create-plan', {
+    //             name: name,
+    //             targetinvestor: targetInvestor,
+    //             targetcash: targetCash,
+    //             targetmultiple: targetMultiple,
+    //             photo: `${response.data.url}`
+    //         })
+    //             .then(response => {
+    //                 console.log(response)
+
+    //                 // Handle successful registration response
+
+    //                 if (response.data['error'] == true) {
+    //                     hideLoader()
+    //                     return Swal.fire({
+    //                         title: "Error",
+    //                         text: response.data['message'],
 
 
-                icon: "error"
-            });
+    //                         icon: "error"
+    //                     });
+    //                 }
+
+    //                 hideLoader()
+
+    //                 return Swal.fire({
+    //                     title: "Success",
+    //                     text: 'Successful',
+
+
+    //                     icon: "success"
+    //                 });
+
+    //             })
+    //             .catch(error => {
+    //                 hideLoader()
+    //                 return Swal.fire({
+    //                     title: "Error",
+    //                     text: error,
+
+
+    //                     icon: "error"
+    //                 });
 
 
 
-        })
-        .finally(() => {
-            // Hide the loader when the request is complete (success or failure)
+    //             })
+    //             .finally(() => {
+    //                 // Hide the loader when the request is complete (success or failure)
+    //                 hideLoader()
+    //             });
+    //     })
+    //     .catch(error => {
+    //         hideLoader()
+    //         return Swal.fire({
+    //             title: "Error",
+    //             text: error,
 
-        });
+
+    //             icon: "error"
+    //         });
+
+
+
+    //     })
+    //     .finally(() => {
+    //         // Hide the loader when the request is complete (success or failure)
+
+    //     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     // Create data object to send in the request
